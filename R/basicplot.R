1852bb2a506baa1b40368a97df29d08250560eed
nathansam <ncooke96@gmail.com>
2019-05-06 16:40:27 +0100

    fix ggsave in basicplot

--- R/basicplot.R
+++ R/basicplot.R
@@ -14,10 +14,10 @@ basicplot <- function(genename, dataset, timelag = 0, save = FALSE, print = TRUE
     genematrix <- subset(dataset, dataset[1] == genename)
     timevector <- (maketimevector(genematrix) - timelag)  # makes timevector
     genematrix <- t(genematrix[-1])  #remove gene name from subset
-    
+
     genedata <- data.frame(timevector, genematrix)
     names(genedata) <- c("timevector", "activity")
-    
+
     mean.list <- rep(0, length((unique(timevector))))
     count <- 1
     for (i in timevector) {
@@ -25,23 +25,23 @@ basicplot <- function(genename, dataset, timelag = 0, save = FALSE, print = TRUE
         mean.list[[count]] <- (mean(genesub$activity))
         count = count + 1
     }
-    
-    
-    
+
+
+
     genedata <- cbind(genedata$timevector, genedata$activity, mean.list)
     genedata <- data.frame(genedata)
     names(genedata) <- c("timevector", "activity", "newmean")
-    graphic <- ggplot2::ggplot(data = genedata, ggplot2::aes(x = timevector, 
-        y = activity)) + ggplot2::geom_line(ggplot2::aes(x = timevector, y = newmean), 
+    graphic <- ggplot2::ggplot(data = genedata, ggplot2::aes(x = timevector,
+        y = activity)) + ggplot2::geom_line(ggplot2::aes(x = timevector, y = newmean),
         size = 1) + ggplot2::geom_point(size = 3, alpha = 0.5, color = "#39A5AE")
-    graphic <- graphic + ggplot2::xlab("Time (Hours)") + ggplot2::ylab("Transcripts Per Million (TPM)") + 
+    graphic <- graphic + ggplot2::xlab("Time (Hours)") + ggplot2::ylab("Transcripts Per Million (TPM)") +
         ggplot2::theme_bw()
-    graphic <- graphic + ggplot2::theme(plot.title = ggplot2::element_text(hjust = 1)) + 
+    graphic <- graphic + ggplot2::theme(plot.title = ggplot2::element_text(hjust = 1)) +
         ggplot2::theme(text = ggplot2::element_text(size = 12))
     graphic <- graphic + ggplot2::ggtitle(paste("Gene = ", genename))
-    
+
     if (save == TRUE) {
-        ggsave(paste(genename, ".png"), graphic, width = 4, heigh = 3, units = "in", 
+        ggplot2::ggsave(paste(genename, ".png"), graphic, width = 4, heigh = 3, units = "in",
             dpi = 600)
     }
     if (print == TRUE) {
