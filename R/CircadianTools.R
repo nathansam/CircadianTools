e321c44a9bf0edb494dc5e8f73a0e9a04fca3c23
nathansam <ncooke96@gmail.com>
2019-05-06 15:49:34 +0100

    Tidy/ update documentation + code

--- R/CircadianTools.R
+++ R/CircadianTools.R
@@ -8,7 +8,8 @@
 #' \code{\link{cosinorplot}}: Fits a cosinor model to a given gene in a given dataset and plots the model. \cr \cr
 #' @section Turning Point Analysis:
 #' \code{\link{turningplot}}: Fits a spline to a given gene in a given dataset. Finds the turning points. Plots the turning points and spline.
-#' @section Clean-Up Functions:
+#' @section Utility Functions:
+#' \code{\link{ggplot.cosinor.lm}}: Adapted from the Cosinor package by Michael Sachs. Given a cosinor.lm model fit, generate a plot of the data with the fitted values.
 #' \code{\link{maketimevector}}: Produces a vector of time values for the gene activity readings. \cr \cr
 #' \code{\link{geneclean}}: Removes columns and rows which show no gene activity over time. \cr \cr
 #'
