9422712687c3026b54711d437c537c4780b3129b
nathansam <ncooke96@gmail.com>
2019-06-13 13:52:51 +0100

    Fix anovafilter. Warning! High Ram Usage

--- R/anovafilter.R
+++ R/anovafilter.R
@@ -2,34 +2,32 @@
 #' filters a genedata via
 #'
 #' @param dataset A transcriptomics dataset. First columns should be gene names. All other columns should be expression levels.
-#'
+#' @param nthreads Number of processor threads for the filtering. Defaults to 2.
 #' @examples
-#' Laurasmappings_filtered <- cosinoranalysis(Laurasmappings)
+#' Laurasmappings_filtered <- anovafilter(Laurasmappings, nthreads=4)
 
 
-anovafilter <- function(dataset){
-  genenumber<-nrow(dataset)
-  dataset<-geneclean(dataset)
-  count<-0
+anovafilter <- function(dataset, nthreads=2) {
+    library(foreach)
+    dataset <- geneclean(dataset)
+    genenumber <- nrow(dataset)
+    count <- 0
 
-  timevector<-maketimevector(dataset)
-  library(foreach)
-  doMC::registerDoMC(4)
-  test3<-foreach::foreach(i=1:genenumber, .combine = rbind) %dopar%{
-      genematrix <- dplyr::filter(dataset, dplyr::row_number() == i)
-      genematrix<-t(genematrix[-1])
+    timevector <- maketimevector(dataset)
+    cl<-parallel::makeForkCluster(parallel::detectCores())
+    doParallel::registerDoParallel(cl)
 
-      test<-data.frame(genematrix, timevector)
-      names(test)<-c("genematrix","timevector")
+    filterdf <- foreach(i = 1:genenumber, .combine = rbind) %dopar% {
+        genematrix <- dplyr::filter(dataset, dplyr::row_number() == i)
+        genematrix <- t(genematrix[-1])
 
-      #print(genematrix)
-      #print(timevector)
-
-    tempaov<-aov(lm(genematrix~timevector, data=test))
-    pvalue<-summary(tempaov)[[1]][1,5]
-    if (pvalue<0.05){
-      dplyr::filter(dataset, dplyr::row_number() == i)
-}
-  }
-  return(test3)
+        test <- data.frame(genematrix, timevector)
+        names(test) <- c("genematrix", "timevector")
+        tempaov <- aov(lm(genematrix ~ timevector, data = test))
+        pvalue <- summary(tempaov)[[1]][1, 5]
+        if (pvalue < 0.05) {
+            dplyr::filter(dataset, dplyr::row_number() == i)
+        }
+    }
+    return(filterdf)
 }
