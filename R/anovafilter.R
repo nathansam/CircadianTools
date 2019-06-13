405d0de6ac958c702569c203b2f04e365d205bc3
nathansam <ncooke96@gmail.com>
2019-06-13 14:11:59 +0100

    Make it so nthreads argument for anovafilter works as intended.

--- R/anovafilter.R
+++ R/anovafilter.R
@@ -2,25 +2,29 @@
 #' filters a genedata via
 #'
 #' @param dataset A transcriptomics dataset. First columns should be gene names. All other columns should be expression levels.
-#' @param nthreads Number of processor threads for the filtering. Defaults to 2.
+#' @param nthreads Number of processor threads for the filtering. If not specifed then the maximum logical cores are used.
 #' @examples
 #' Laurasmappings_filtered <- anovafilter(Laurasmappings, nthreads=4)
 
 
-anovafilter <- function(dataset, nthreads=2) {
+anovafilter <- function(dataset, nthreads = NULL) {
     library(foreach)
+    if (is.null(nthreads) == TRUE) {
+        nthreads <- parallel::detectCores()
+    }
+    
     dataset <- geneclean(dataset)
     genenumber <- nrow(dataset)
     count <- 0
-
+    
     timevector <- maketimevector(dataset)
-    cl<-parallel::makeForkCluster(parallel::detectCores())
+    cl <- parallel::makeForkCluster(nthreads)
     doParallel::registerDoParallel(cl)
-
+    
     filterdf <- foreach(i = 1:genenumber, .combine = rbind) %dopar% {
         genematrix <- dplyr::filter(dataset, dplyr::row_number() == i)
         genematrix <- t(genematrix[-1])
-
+        
         test <- data.frame(genematrix, timevector)
         names(test) <- c("genematrix", "timevector")
         tempaov <- aov(lm(genematrix ~ timevector, data = test))
