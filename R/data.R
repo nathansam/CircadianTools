63e605c9b3d97ff92087615221398e7e547a3798
Nathan <ncooke96@gmail.com>
2019-10-31 21:03:50 +0000

    Add data

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Gene expression count data of \emph{T. saltator}.
+#'
+#' A transcriptomics dataset containing the gene expression counts for 156767
+#' genes of \emph{T. saltator} across 21 hours. First reading is at hour 6.
+#' Each time point has 4 replicates with the exception of hour 18 which has 3.
+#'
+#'
+#' @format A data frame with 91311 rows and 32 variables:
+#' \describe{
+#'   \item{sample}{Sample ID}
+#'   \item{CT6.2}{Gene expression count at t = 6, replicate 1}
+#'   \item{CT6.3}{Gene expression count at t = 6, replicate 2}
+#'   ...
+#'   \item{CT27.5}{Gene expression count at t = 27, replicate 4}
+#' }
+#' @source \url{http://doi.org/10.7717/peerj.2555}
+"Laurasmappings"
